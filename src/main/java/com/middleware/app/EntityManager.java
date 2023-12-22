ebf3c1872dc07571910f36dfb8bc028ee46efaaa
faceslog <66552663+faceslog@users.noreply.github.com>
2023-12-22 12:26:38 +0100

    Add EntityManager

---
+++ src/main/java/com/middleware/app/EntityManager.java
@@ -0,0 +1,51 @@
+package com.middleware.app;
+
+import com.middleware.app.game.bosses.Boss;
+import com.middleware.app.game.bosses.IceDragon;
+import com.middleware.app.game.players.LightGuardian;
+import com.middleware.app.network.NetworkPlayer;
+
+import java.util.*;
+
+public class EntityManager {
+
+    private final Integer currentPlayerId;
+    private final Map<Integer, LightGuardian> heroes;
+    private final Map<Integer, NetworkPlayer> players;
+    private final IceDragon boss;
+
+    public EntityManager(Integer currentPlayerId, List<NetworkPlayer> players) {
+
+        this.currentPlayerId = currentPlayerId;
+
+        this.players = new HashMap<>();
+        this.heroes = new HashMap<>();
+
+        for (NetworkPlayer player : players) {
+            this.players.put(player.getPlayerId(), player);
+            this.heroes.put(player.getPlayerId(), new LightGuardian());
+        }
+
+        this.boss = new IceDragon();
+    }
+
+    public NetworkPlayer getCurrentPlayer() {
+        return players.get(currentPlayerId);
+    }
+
+    public LightGuardian getCurrentHero() {
+        return heroes.get(currentPlayerId);
+    }
+
+    public NetworkPlayer getPlayerById(Integer id) {
+        return players.get(currentPlayerId);
+    }
+
+    public LightGuardian getHeroById(Integer id) {
+        return heroes.get(id);
+    }
+
+    public Boss getBoss() {
+        return boss;
+    }
+}
